c7a5df5e53acfd6b8d91c145c16964655d014f5e
Victor Torres <vatorresm88@gmail.com>
2018-01-21 18:01:24 -0500

    Avance del Blog en Laravel

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\PostStoreRequest;
+use App\Http\Requests\PostUpdateRequest;
+
+use App\Http\Controllers\Controller;
+
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::orderBy('id', 'DESC')->paginate();
+
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostStoreRequest $request)
+    {
+        $Post = Post::create($request->all());
+
+        return redirect()->route('posts.edit', $Post->id)->with('info', 'Entrada creada con éxito');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $Post = Post::find($id);
+
+        return view('admin.posts.show', compact('Post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $Post = Post::find($id);
+
+        return view('admin.posts.edit', compact('Post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostUpdateRequest $request, $id)
+    {
+        $Post = Post::find($id);
+
+        $Post->fill($request->all())->save();
+
+        return redirect()->route('posts.edit', $Post->id)->with('info', 'Entrada actualizada con éxito');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $Post = Post::find($id)->delete();
+
+        return back()->with('info', 'Eliminado correctamente');
+    }
+}
